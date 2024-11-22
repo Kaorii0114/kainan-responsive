9a396ad9d3744decfc8f90ec3b63c58435303ec8
kaori <klarkrheinonate@gmail.com>
2024-11-22 23:38:48 +0800

    try

---
+++ js/header-footer.js
@@ -0,0 +1,28 @@
+window.addEventListener("scroll", function(){
+    var navbar = document.querySelector("header");
+    navbar.classList.toggle("sticky", window.scrollY > 0);
+})
+function redirectToMenu(){  
+    window.location.href = "menu.html";
+}
+function redirectToFacebook(){
+    window.location.assign("https://www.facebook.com/people/Kainan-En-La-Kanto/61568619036341/");
+}
+function redirectToDiscord(){
+    window.location.assign("https://www.facebook.com/khirstenadrian.flora.5");
+}
+function redirectToInstagram(){
+    window.location.assign("https://www.instagram.com/kainan_en_la_kanto/?fbclid=IwZXh0bgNhZW0CMTEAAR2ViMAD1sJm4H_ZP6yynwJCnDnlCBEW4k22c2O4pVwo2AarL4-3M7tsGug_aem_0ubQGQwAoowBTMGbfEGCIA");
+}
+function redirectToTwitter(){
+    window.location.assign(" ");
+}
+function igPost1(){
+    window.location.assign("https://www.instagram.com/p/DCjqT8xyMOC/?igsh=ZndjazZ6ZHV0b2Yz&fbclid=IwZXh0bgNhZW0CMTEAAR063upbWIFXa-3m5Yb6PB9A2QR-_cp2htlqh616--ViEL-aILoIr9DwP1s_aem_HU9rb0SdUKb7eUIXpWHrUg");
+}
+function igPost2(){
+    window.location.assign("https://www.instagram.com/p/DCjqGGRydBb/?igsh=ZDcwb2lnZzU0Y2kx&fbclid=IwZXh0bgNhZW0CMTEAAR0HcoSkwCyec9tOOvO7uYOnK9w3g7ddffZvid_87LMhqaJZfjPc8rqKbjA_aem_06QHfVDi85l_zn1lRIDe8w");
+}
+function igPost3(){
+    window.location.assign("https://www.instagram.com/p/DCjqKnWyOMJ/?igsh=MXkwcDBqMjdmY2huNw%3D%3D&fbclid=IwZXh0bgNhZW0CMTEAAR1hrkVa3nYUg0NeRmIAMIgNyLs0-0EvAIy7O9xNe0P-zqROI6RhVZXgPxk_aem__Agv1hYIRazAu3yVG84Yvw");
+}
